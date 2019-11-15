0c21ccf6330a838231f69f369b4d716f6c1498a2
yeison bedoya <bedoyadt@gmail.com>
2019-11-15 12:18:43 -0500

    pru7

--- app/src/main/java/com/example/apptiendad/CuentaUsuarioActivity.java
+++ app/src/main/java/com/example/apptiendad/CuentaUsuarioActivity.java
@@ -1,7 +1,5 @@
 package com.example.apptiendad;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -10,7 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.example.apptiendad.model.cliente;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class CuentaUsuarioActivity extends AppCompatActivity {
